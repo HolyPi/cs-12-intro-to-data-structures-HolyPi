5bf5b0a8c201fb80581ee338818cdb942a633154
HolyPi <joanelly2018@gmail.com>
2020-01-29 14:05:59 -0800

    Dictionary_words added

---
+++ histogram.py
@@ -0,0 +1,22 @@
+
+filename ="text.txt"
+lines = open(filename, "r")
+words = lines.read()
+
+wordslist = words.splitlines()
+
+text_histogram = {}
+
+
+
+def histogram():
+ 
+
+
+
+
+histogram()
+print(text_histogram)
+
+
+
