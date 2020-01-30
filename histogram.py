62b9aa6d875078977e3eb7931e3f1d4f9b4234ce
HolyPi <joanelly2018@gmail.com>
2020-01-29 20:30:31 -0800

    Added histogram:

--- histogram.py
+++ histogram.py
@@ -3,16 +3,20 @@ filename ="text.txt"
 lines = open(filename, "r")
 words = lines.read()
 
-wordslist = words.splitlines()
+wordslist = words.split()
 
 text_histogram = {}
 
 
 
 def histogram():
- 
 
-
+    for word in wordslist:
+        word = word.strip(',)-(?.\"\'!').lower()
+        if word not in text_histogram.keys():
+            text_histogram[word]= 1 
+        else:  
+            text_histogram[word] += 1
 
 
 histogram()
